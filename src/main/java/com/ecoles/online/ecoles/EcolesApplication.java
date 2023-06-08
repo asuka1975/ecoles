68ce7ef444307dae35738570d3b547dbd1e3f248
hungr <hungryfactory@icloud.com>
2023-06-08 20:51:40 +0900

    initial commit

---
+++ src/main/java/com/ecoles/online/ecoles/EcolesApplication.java
@@ -0,0 +1,13 @@
+package com.ecoles.online.ecoles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolesApplication.class, args);
+	}
+
+}
