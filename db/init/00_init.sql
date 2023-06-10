79f2227d4911f5c58131fa98ab55056971bbd904
hungr <hungryfactory@icloud.com>
2023-06-10 22:03:12 +0900

    feat: dummy data initialization

--- db/init/00_init.sql
+++ db/init/00_init.sql
@@ -32,6 +32,7 @@ CREATE TABLE IF NOT EXISTS customer (
     id          UUID DEFAULT uuid_generate_v4(),
     name        TEXT NOT NULL,
     email       TEXT NOT NULL UNIQUE,
+    password    CHAR(128) NOT NULL,
     created_at  DATE NOT NULL,
     PRIMARY KEY (id)
 );
---
+++ db/init/01_data.sql
@@ -0,0 +1,3 @@
+\c library;
+
+\COPY author FROM '/data/author.csv' DELIMITER ',' CSV HEADER;
