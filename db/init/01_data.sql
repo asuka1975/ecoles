88a621ff308def24ba6073ce0f54642bf9086411
hungr <hungryfactory@icloud.com>
2023-06-10 22:15:11 +0900

    feat: 書籍詳細情報のテーブルを初期化

--- db/init/01_data.sql
+++ db/init/01_data.sql
@@ -1,3 +1,5 @@
 \c library;
 
 \COPY author FROM '/data/author.csv' DELIMITER ',' CSV HEADER;
+\COPY book FROM '/data/book.csv' DELIMITER ',' CSV HEADER;
+\COPY book_detail FROM '/data/book_detail.csv' DELIMITER ',' CSV HEADER;
